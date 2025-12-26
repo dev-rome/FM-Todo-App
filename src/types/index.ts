5ba2070f076e6d0d5ab4f4a4621d1c2d6eae3161
dev-rome <info.devrome@gmail.com>
2025-12-26 16:40:17 -0500

    add TodoFormProps, TodoListProps, and TodoItemProps

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,23 @@
+export interface ThemeContextType {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
 export interface TodoItems {
   id: string;
   task: string;
   isCompleted: boolean;
 }
-
-export interface ThemeContextType {
-  theme: "light" | "dark";
-  toggleTheme: () => void;
+export interface TodoFormProps {
+  addTodos: (todoTask: string) => void;
+}
+export interface TodoListProps {
+  todos: TodoItems[];
+  toggleTodos: (id: string) => void;
+  removeTodos: (id: string) => void;
+}
+export interface TodoItemProps {
+  title: string;
+  checked: boolean;
+  onToggleChange: () => void;
+  onDelete: () => void;
 }
