c797b468d09d62597caa9dac438be7b2ed312057
dev-rome <info.devrome@gmail.com>
2025-12-25 23:15:01 -0500

    todo app project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main>
+        <div className="bg-red-500">
+          <h1>Todo App</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
