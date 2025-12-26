00aa28b51761977dfdb6db86548d02ecab3c16b3
dev-rome <info.devrome@gmail.com>
2025-12-26 13:52:19 -0500

    add useState to manage todo state and added addTodo, toggleTodo, removeTodo functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { TodoItems } from "./types";
+import TodoForm from "./components/TodoForm";
+
 function App() {
+  const [todos, setTodos] = useState<TodoItems[]>([]);
+
+  const addTodo = (task: string) => {
+    const newTodo: TodoItems = {
+      id: uuidv4(),
+      task,
+      isCompleted: false,
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const toggleTodoCompletion = (id: string) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    )
+  }
+
+  const removeTodo = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <>
       <main>
-        <div className="bg-red-500">
-          <h1>Todo App</h1>
+        <div>
+          <TodoForm />
         </div>
       </main>
     </>
