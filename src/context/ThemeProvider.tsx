0cfe4baf32da3b302f3b157010b401d9b9798942
dev-rome <info.devrome@gmail.com>
2025-12-26 22:22:05 -0500

    update ThemeProvider to fix theme

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -2,19 +2,19 @@ import { useState, useEffect } from "react";
 import { ThemeContext } from "./ThemeContext";
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    const [theme, setTheme] = useState<'light' | 'dark'>("light");
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
-    const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-    }
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
-    useEffect(() => {
-        document.documentElement.setAttribute("data-theme", theme);
-    }, [theme]);
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
 
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-}
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
