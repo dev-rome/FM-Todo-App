c352c6420a3aae591ac6c0a6ba8b2620e41ceffc
dev-rome <info.devrome@gmail.com>
2025-12-26 23:31:42 -0500

    create header component with useTheme for theme toggle

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from "../hooks/useTheme";
+import Moon from "../assets/images/icon-moon.svg";
+import Sun from "../assets/images/icon-sun.svg";
+
+export default function Header() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <header className="flex items-center justify-between px-6 pt-12 md:mx-auto md:max-w-[540px]">
+      <p className="text-2xl font-bold tracking-[0.3em] text-white">TODO</p>
+      <button className="lg:cursor-pointer" onClick={toggleTheme}>
+        <img
+          src={theme === "dark" ? Sun : Moon}
+          alt={theme === "dark" ? "sun icon" : "moon icon"}
+          className="w-[19px] md:w-[26px]"
+        />
+      </button>
+    </header>
+  );
+}
